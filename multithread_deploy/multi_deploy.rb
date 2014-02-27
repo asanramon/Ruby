7896132a13819fee068650e65b96d71c1b1d7bb2
Angelo San Ramon <AngeloSanRamon@gmail.com>
2014-02-27 01:25:07 -0800

    initial commit

---
+++ multithread_deploy/multi_deploy.rb
@@ -0,0 +1,48 @@
+#!/usr/bin/ruby
+require 'thread'
+require 'logger'
+require 'net/scp'
+#require 'net/ping'
+
+def func(host)
+  log = Logger.new("#{host}.log")
+  log.info("pinging #{host}...")
+  if ENV['OS'].eql?("Windows_NT")
+    ping_result = `ping -n 1 #{host}`
+    if ping_result.include?("Request timed out") || ping_result.include?("host unreachable")
+      log.error "ping failed. exiting..."
+      return
+    else
+      log.info "ping successful."
+    end
+  else
+    ping_result = `ping -c 1 #{host}`
+    if $?.exitstatus != 0
+      log.error "ping failed. exiting..."
+      return
+    else
+      log.info "ping successful."
+    end
+  end
+  Net::SCP.start(host,"root",:password => "password") do |scp|
+    scp.upload! "/source/file/path", "/destination/file/path"
+  end
+end
+
+#main
+max_concurrent_threads = 10
+hosts_file = File.open("hosts.txt","r")
+hosts_file.each_line do |line|
+  thr = Thread.new do
+    func(line.chomp)
+  end
+  #wait until total threads goes below maximum allowed threads
+  while Thread.list.size > max_concurrent_threads do
+    sleep 1
+  end
+end
+#wait for the rest of the threads to finish
+while Thread.list.size > 1 do
+  sleep 1
+end
+hosts_file.close
